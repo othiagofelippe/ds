e249545ac37727fe2ef84287f7f37ef985c41f42
öthiagofelippe <thiagofelippe135@gmail.com>
2025-11-25 15:52:31 -0300

    feat: add typography tokens and update index export

---
+++ src/tokens/typography.ts
@@ -0,0 +1,57 @@
+export const typography = {
+  fontFamily: {
+    poppins: "var(--font-poppins)",
+    roboto: "var(--font-roboto)",
+  },
+
+  "6xl": {
+    fontSize: 60,
+    lineHeight: 60,
+    fontWeight: 700,
+  },
+  "5xl": {
+    fontSize: 48,
+    lineHeight: 48,
+    fontWeight: 500,
+  },
+  "4xl": {
+    fontSize: 36,
+    lineHeight: 40,
+    fontWeight: 500,
+  },
+  "3xl": {
+    fontSize: 30,
+    lineHeight: 36,
+    fontWeight: 500,
+  },
+  "2xl": {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 500,
+  },
+  xl: {
+    fontSize: 20,
+    lineHeight: 28,
+    fontWeight: 500,
+  },
+  lg: {
+    fontSize: 18,
+    lineHeight: 28,
+    fontWeight: 500,
+  },
+  base: {
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: 400,
+  },
+  sm: {
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: 400,
+  },
+  xs: {
+    fontSize: 12,
+    lineHeight: 16,
+    fontWeight: 300,
+  },
+};
